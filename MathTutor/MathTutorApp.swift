61a3ae2a2685305cbfca86cd3f89434f4e4167ee
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-17 07:52:14 -0500

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Tushar Munge on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
